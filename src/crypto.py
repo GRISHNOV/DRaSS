40beb0619d4b8b10db4336f833d796aa8a861a1a
GRISHNOV <grishnov98@mail.ru>
2019-12-02 23:40:49 +0300

    4-th

--- src/crypto.py
+++ src/crypto.py
@@ -2,12 +2,29 @@
 
 import hashlib
 import binascii
+import time
+
 
 
 print("\n---------- START module crypto ----------\n\n")
 
+begin = [0]
+begin[0] = time.time()
+
 hash = hashlib.sha256(b'test').hexdigest()
+hash_iteration = 1
+iteration_constant = 10**6
+
+while (hash_iteration < iteration_constant):
+    hash = hashlib.sha256(hash.encode()).hexdigest()
+    hash_iteration = hash_iteration + 1
+
 print(hash)
+end = time.time()
+elapsed = end - begin[0]
+print(elapsed)
+
+input()
 
 hash_crc = binascii.crc32(hash.encode())
 print(hash_crc)
