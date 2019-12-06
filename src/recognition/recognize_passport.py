734c6a0080ab3b4d26c439744d2b42ef3d459bc5
mikhan333 <mikhan333@mail.ru>
2019-12-07 01:35:56 +0300

    Fix style of code and impore recognize

--- src/recognition/recognize_passport.py
+++ src/recognition/recognize_passport.py
@@ -6,70 +6,131 @@ from passporteye import read_mrz
 from collections import OrderedDict
 
 
-def translate(name):
-    slovar = {
-        'A': 'А', 'B': 'Б',	'V': 'В', 'G': 'Г',	
-        'D': 'Д', 'E': 'Е',	'2': 'Ё', 'J': 'Ж',	
-        'Z': 'З', 'I': 'И',	'Q': 'Й', 'K': 'К',	
-        'L': 'Л', 'M': 'М',	'N': 'Н', 'O': 'О',	
-        'P': 'П', 'R': 'Р', 'S': 'С', 'T': 'Т',
-        'U': 'У', 'F': 'Ф', 'H': 'Х', 'C': 'Ц',
-        '3': 'Ч', '4': 'Ш', 'W': 'Щ', 'X': 'Ъ',
-        'Y': 'Ы', '9': 'Ь', '6': 'Э', '7': 'Ю',
-        '8': 'Я'
-    }
-        
-    for key in slovar:
-        name = name.replace(key, slovar[key])
-    return name
+class Recognize:
 
+    def __init__(self):
+        self._map_latin_to_cyrillic = {
+            'A': 'А', 'B': 'Б',	'V': 'В',
+            'G': 'Г', 'D': 'Д', 'E': 'Е',
+            '2': 'Ё', 'J': 'Ж',	'Z': 'З',
+            'I': 'И', 'Q': 'Й', 'K': 'К',
+            'L': 'Л', 'M': 'М',	'N': 'Н',
+            'O': 'О', 'P': 'П', 'R': 'Р',
+            'S': 'С', 'T': 'Т', 'U': 'У',
+            'F': 'Ф', 'H': 'Х', 'C': 'Ц',
+            '3': 'Ч', '4': 'Ш', 'W': 'Щ',
+            'X': 'Ъ', 'Y': 'Ы', '9': 'Ь',
+            '6': 'Э', '7': 'Ю', '8': 'Я',
+        }
+        self._map_sex = {
+            'M': 'МУЖ.',
+            'F': 'ЖЕН.',
+        }
+        self._max_year_20th = 50
 
-def get_correct_day(day):
-    return day[4:] + '.' + day[2:4] + '.' + \
-        ('20' if int(day[:2]) < 50 else '19') + day[:2]
+    def recongize_file(self, path_to_file):
+        fd = open(path_to_file, 'rb')
+        return self.recognize_fd(fd)
 
+    def recognize_fd(self, data):
+        recogn_data = self.get_mrz(data).to_dict()
 
-def recognize_data(data):
-    mrz = read_mrz(data, extra_cmdline_params='--oem 0')
-    recogn_data = mrz.to_dict()
-    print(recogn_data)
+        name, patronymic = recogn_data['names'].split()
+        correct_data = OrderedDict({
+            'type':         recogn_data['type'],
+            'country':      recogn_data['country'],
+            'nationality':  recogn_data['nationality'],
 
-    name, patronymic = recogn_data['names'].split()
-    correct_data = OrderedDict({
-        'type': recogn_data['type'],
-        'country': recogn_data['country'],
-        'nationality': recogn_data['nationality'],
-        'surname': translate(recogn_data['surname']),
-        'name': translate(name),
-        'patronymic': translate(patronymic),
-        'sex': 'МУЖ.' if recogn_data['sex'] == 'M' else 'ЖЕН.'
-    })
+            'surname':      self._translate_latin_to_cyrillic(recogn_data['surname']),
+            'name':         self._translate_latin_to_cyrillic(name),
+            'patronymic':   self._translate_latin_to_cyrillic(patronymic),
+            'sex':          self._map_sex[recogn_data['sex']],
+        })
 
-    if recogn_data['valid_date_of_birth']:
-        correct_data['birth_day'] = get_correct_day(recogn_data['date_of_birth']) 
-    
-    if recogn_data['valid_number']:
-        correct_data['passport_number'] = recogn_data['number'][3:9]
+        if recogn_data['valid_date_of_birth']:
+            correct_data['birth_day'] = self._get_day_correct_view(
+                recogn_data['date_of_birth'])
 
-    if recogn_data['valid_personal_number']:
         if recogn_data['valid_number']:
-            correct_data['passport_series'] = recogn_data['number'][0:3] + recogn_data['personal_number'][0]
-        correct_data['extradition_day'] = get_correct_day(recogn_data['personal_number'][1:7])
-        correct_data['unit_code'] = recogn_data['personal_number'][7:10] + '-' + recogn_data['personal_number'][10:13]
-    
-    print(correct_data)
-    return recogn_data
+            correct_data['passport_number'] = recogn_data['number'][3:9]
+
+        if recogn_data['valid_personal_number']:
+            if recogn_data['valid_number']:
+                correct_data['passport_series'] = recogn_data['number'][0:3] + \
+                    recogn_data['personal_number'][0]
+            correct_data['extradition_day'] = self._get_day_correct_view(
+                recogn_data['personal_number'][1:7])
+            correct_data['unit_code'] = recogn_data['personal_number'][7:10] + \
+                '-' + recogn_data['personal_number'][10:13]
+
+        return correct_data
+
+    def get_mrz(self, data):
+        mrz = read_mrz(data, extra_cmdline_params='--oem 0')
+        return mrz
+
+    def _translate_latin_to_cyrillic(self, word):
+        for char in self._map_latin_to_cyrillic:
+            word = word.replace(char, self._map_latin_to_cyrillic[char])
+        return word
+
+    def _get_day_correct_view(self, day_number):
+        return \
+            day_number[4:] + '.' + \
+            day_number[2:4] + '.' + \
+            ('20' if int(day_number[:2]) <
+             self._max_year_20th else '19') + day_number[:2]
+
+
+def test():
+    for file, test_file in [('./tests/data/passport-td3.jpg', assert_td3_jpg),
+                            ('./tests/data/passport-td3.png', assert_td3_png),
+                            ('./tests/data/passport-td2.jpg', assert_td2_jpg),
+                            ('./tests/data/passport-td2.png', assert_td2_png),
+                            ]:
+        for as_stream in [True, False]:
+            test_fn(read_img(fn, as_stream=as_stream))
+
+
+class TestAction(argparse.Action):
+
+    def __init__(self,
+                 option_strings,
+                 dest='test',
+                 default=False,
+                 help=None):
+        super().__init__(
+            option_strings=option_strings,
+            dest=dest,
+            default=default,
+            nargs=0,
+            help=help)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        test()
+        parser.exit()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Function for recognition of passport and cards')
-    parser.add_argument('infile', type=argparse.FileType('rb'), help='input file with text for recognition')
-    parser.add_argument('-t', '--test', action='store_true', help='provide testing this function and exit')
+    parser = argparse.ArgumentParser(
+        description='Function for recognition of passport and cards'
+    )
+    parser.add_argument(
+        'infile',
+        type=argparse.FileType('rb'),
+        help='input file with text for recognition'
+    )
+    parser.add_argument(
+        '-t', '--test',
+        action=TestAction,
+        help='test this function and exit'
+    )
+    args = parser.parse_args(sys.argv[1:])
 
-    args = parser.parse_args()
+    recogniser = Recognize()
+    data = recogniser.recognize_fd(args.infile)
+    print(data)
 
-    recognize_data(args.infile)
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
