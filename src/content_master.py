e138666164f7bdd49408ce304770d297dae0e3ef
Denis Stasyev <denisstasyev@gmail.com>
2019-12-10 10:11:57 +0300

    development of the master wizard for working with existing repositories

--- src/content_master.py
+++ src/content_master.py
@@ -40,7 +40,51 @@ if __name__ == "__main__":
 
     print("Соединение с хранилищем установлено...")
     time.sleep(2)
-    
+
+    cursor.execute('SELECT * FROM key_data ')
+    rows = cursor.fetchall()
+
+    ################################
+    # rows[0][0] ==> user_db_name  #
+    # rows[0][1] ==> UK_hash       #
+    # rows[0][2] ==> MK_encypted   #
+    # rows[0][3] ==> MK_CRC        #
+    # rows[0][4] ==> text_comment  #
+    ################################
+
+    ascii_interface.clr_terminal()    
+    while(True):
+
+        ascii_interface.man()
+        print("\t\t\t\tХранилище: ", storage_name)
+        print("\t\t\t\tВведите пароль от хранилища:")
+        UK_input = input() # pUoSi
+        if UK_input == "exit":
+            exit(0)
+        UK_hash = crypto.get_sha256( crypto.get_sha256( UK_input, False) , True )
+
+        if UK_hash == rows[0][1]:
+            ascii_interface.clr_terminal()
+            print("\t\t\t\t\t\tПринято!")
+            ascii_interface.man()
+            break
+            
+        else:
+            ascii_interface.clr_terminal()
+            print("\t\t\t\t\t\t\tОшибка. Повторите ввод")
+
+
+    UK_gamma = crypto.get_sha256( UK_input, False)
+    MK = crypto.get_XOR_cipher( rows[0][2], UK_gamma )
+  
+    if str(crypto.get_crc32(MK)) == rows[0][3]:
+        print()
+        #print("MK CRC correct")
+    else:
+        print("MK CRC error")
+        print("Гаммирование MK завершилось ошибкой. Хранилище повреждено...")
+        exit(0)
+
     connection.commit()
     connection.close()
 
