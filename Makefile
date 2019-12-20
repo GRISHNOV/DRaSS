6953372f4fa9da1c116269474c36bb1e288b3c20
Denis Stasyev <denisstasyev@gmail.com>
2019-12-20 10:15:12 +0300

    Add xterm installation into Makefile

--- Makefile
+++ Makefile
@@ -5,11 +5,14 @@ all: install
 install:
 	sudo apt install tesseract-ocr
 	wget https://github.com/tesseract-ocr/tessdata/raw/master/eng.traineddata
-	sudo mv eng.traineddata /usr/share/tesseract-ocr/4.00/tessdata/eng.traineddata 
+	sudo mv eng.traineddata /usr/share/tesseract-ocr/4.00/tessdata/eng.traineddata
+
+	sudo apt install xterm 
 
 	python3 -m venv env
 
 start:
+	# cd src && python3 run.py
 	python3 src/run.py
 
 end:
@@ -19,6 +22,7 @@ clean:
 
 uninstall: clean
 	sudo apt remove tesseract-ocr
+	sudo apt remove xterm 
 
 info:
 	pip freeze
--- src/interface_keyboard_entropy.py
+++ src/interface_keyboard_entropy.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
 
     print("\n---------- START module interface keyboard entropy ----------\n\n")
 
-
     print("Нажмите несколько случайных клавиш на клавиатуре, это нужно для создания ключей.")
     print("Продолжайте ввод, пока не исчезнет дополнительное окно терминала...")
 
@@ -16,9 +15,8 @@ if __name__ == "__main__":
     sock = socket.socket()
     sock.bind(('', 2019))
     sock.listen(1)
-    os.system("xterm -e './core_keyboard_entropy.py'") # sudo apt install xterm 
+    os.system("xterm -e './core_keyboard_entropy.py'")
     conn, addr = sock.accept()
-
 
     #print (' (debug info) connected:', addr)
 
@@ -41,7 +39,7 @@ if __name__ == "__main__":
     print(entropy_parametr[0])
     for i in entropy_parametr[0]:
         entropy_array.append(i)
-    for k in range(1,10):
+    for k in range(1, 10):
         entropy_array.append(k)
 
     print()
@@ -50,8 +48,8 @@ if __name__ == "__main__":
     buf = 0
     j = 0
 
-    for i in range(2,19):
-        #print(entropy_array[i])
+    for i in range(2, 19):
+        # print(entropy_array[i])
         buf = buf + (int(entropy_array[i])+i)**3
         j = j + 1
         if (j % 3 == 0):
@@ -61,14 +59,14 @@ if __name__ == "__main__":
     print(keys_map_code)
     print()
 
-    map = [ '0','1','2','3','4','5','6','7','8','9',
-            'a','b','c','d','e','f','g','h','i','j',
-            'k','l','m','n','o','p','q','r','s','t',
-            'u','v','w','x','y','z','A','B','C','D',
-            'E','F','G','H','I','J','K','L','M','N',
-            'O','P','Q','R','S','T','U','V','W','X',
-            'Y','Z' ]
-        
+    map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+           'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+           'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+           'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+           'Y', 'Z']
+
     print(map)
     print()
 
@@ -76,7 +74,7 @@ if __name__ == "__main__":
         key.append(map[i])
     print()
 
-    print("KEY:",key)
+    print("KEY:", key)
 
     print("\nPress for push msg to server")
     input()
@@ -90,6 +88,4 @@ if __name__ == "__main__":
     sock.send(data_for_server.encode())
     sock.close()
 
-
     print("\n\n---------- END module intefrace keyboard entropy ----------\n")
-
